d7fd7e88569cdb2e674abd3aab8114e5cbc409de
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-02-23 17:52:28 +0100

    Fix payload content in ZPub example

--- examples/src/main/java/io/zenoh/ZPub.java
+++ examples/src/main/java/io/zenoh/ZPub.java
@@ -25,12 +25,12 @@ public class ZPub {
             try (KeyExpr keyExpr = KeyExpr.tryFrom("demo/example/zenoh-java-pub")) {
                 System.out.println("Declaring publisher on '" + keyExpr + "'...");
                 try (Publisher publisher = session.declarePublisher(keyExpr).res()) {
-                    String payload = "Pub from Java!";
                     System.out.println("Press CTRL-C to quit...");
-                    int idx = 1;
+                    int idx = 0;
                     while (true) {
                         Thread.sleep(1000);
-                        System.out.println("Putting Data ('" + keyExpr + "': '[" + String.format("%4s", idx) + "] " + payload + "')...");
+                        String payload = String.format("[%4s] Pub from Java!", idx);
+                        System.out.println("Putting Data ('" + keyExpr + "': '"+payload+"')...");
                         publisher.put(payload).res();
                         idx++;
                     }
