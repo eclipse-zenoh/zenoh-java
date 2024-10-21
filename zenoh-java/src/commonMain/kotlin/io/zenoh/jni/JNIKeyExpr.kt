7f372b9c5afe9dbb5a697cc918d9106d4e7e9a62
Darius Maitia <darius@zettascale.tech>
2024-10-21 14:06:13 -0300

    Alignment: JNIKeyExpr

--- zenoh-java/src/commonMain/kotlin/io/zenoh/jni/JNIKeyExpr.kt
+++ zenoh-java/src/commonMain/kotlin/io/zenoh/jni/JNIKeyExpr.kt
@@ -14,22 +14,23 @@
 
 package io.zenoh.jni
 
-import io.zenoh.Zenoh
+import io.zenoh.ZenohLoad
 import io.zenoh.exceptions.ZError
 import io.zenoh.keyexpr.KeyExpr
+import io.zenoh.keyexpr.SetIntersectionLevel
 
 internal class JNIKeyExpr(internal val ptr: Long) {
 
     companion object {
-        @Throws(ZError::class)
+        init {
+            ZenohLoad
+        }
+
         fun tryFrom(keyExpr: String): KeyExpr {
-            Zenoh.load() // It may happen the zenoh library is not yet loaded when creating a key expression.
             return KeyExpr(tryFromViaJNI(keyExpr))
         }
 
-        @Throws(ZError::class)
         fun autocanonize(keyExpr: String): KeyExpr {
-            Zenoh.load()
             return KeyExpr(autocanonizeViaJNI(keyExpr))
         }
 
@@ -47,17 +48,44 @@ internal class JNIKeyExpr(internal val ptr: Long) {
             keyExprB.keyExpr
         )
 
-        @Throws(Exception::class)
+        fun relationTo(keyExpr: KeyExpr, other: KeyExpr): SetIntersectionLevel {
+            val intersection = relationToViaJNI(
+                keyExpr.jniKeyExpr?.ptr ?: 0,
+                keyExpr.keyExpr,
+                other.jniKeyExpr?.ptr ?: 0,
+                other.keyExpr
+            )
+            return SetIntersectionLevel.fromInt(intersection)
+        }
+
+        fun joinViaJNI(keyExpr: KeyExpr, other: String): KeyExpr {
+            return KeyExpr(joinViaJNI(keyExpr.jniKeyExpr?.ptr ?: 0, keyExpr.keyExpr, other))
+        }
+
+        fun concatViaJNI(keyExpr: KeyExpr, other: String): KeyExpr {
+            return KeyExpr(concatViaJNI(keyExpr.jniKeyExpr?.ptr ?: 0, keyExpr.keyExpr, other))
+        }
+
+        @Throws(ZError::class)
         private external fun tryFromViaJNI(keyExpr: String): String
 
-        @Throws(Exception::class)
+        @Throws(ZError::class)
         private external fun autocanonizeViaJNI(keyExpr: String): String
 
-        @Throws(Exception::class)
+        @Throws(ZError::class)
         private external fun intersectsViaJNI(ptrA: Long, keyExprA: String, ptrB: Long, keyExprB: String): Boolean
 
-        @Throws(Exception::class)
+        @Throws(ZError::class)
         private external fun includesViaJNI(ptrA: Long, keyExprA: String, ptrB: Long, keyExprB: String): Boolean
+
+        @Throws(ZError::class)
+        private external fun relationToViaJNI(ptrA: Long, keyExprA: String, ptrB: Long, keyExprB: String): Int
+
+        @Throws(ZError::class)
+        private external fun joinViaJNI(ptrA: Long, keyExprA: String, other: String): String
+
+        @Throws(ZError::class)
+        private external fun concatViaJNI(ptrA: Long, keyExprA: String, other: String): String
     }
 
     fun close() {
